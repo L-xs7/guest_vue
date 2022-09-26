978be3a995cd41650c6f460ad978aa5943bc225b
lixuesong <lixuesong@china-gold.com>
2022-09-26 16:28:33 +0800

    菜单页面bug调整

--- src/api/config/menu.js
+++ src/api/config/menu.js
@@ -9,10 +9,26 @@ export function menuTree() {
     })
 }
 
+//菜单list
+export function menuList() {
+    return Axios({
+        url: '/menu/selMenuList',
+        method: 'get',
+    })
+}
+
 //菜单名查询
 export function menuByName(menuName) {
     return Axios({
         url: `/menu/selMenuByName?menuName=${menuName}`,
+        method: 'get',
+    })
+}
+
+//根据id查询
+export function menuById(id) {
+    return Axios({
+        url: `/menu/selMenuById?id=${id}`,
         method: 'get',
     })
 }
@@ -22,5 +38,14 @@ export function menuDel(id){
     return Axios({
         url: `/menu/delMenu?id=${id}`,
         method: 'get',
+    })
+}
+
+//保存或修改菜单
+export function menuSaveOrUpd(data){
+    return Axios({
+        url: `/menu/operationMenu`,
+        method: 'post',
+        data
     })
 }
